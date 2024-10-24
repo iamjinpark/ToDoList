df08e3a33a096c1e427133833ffe37632613a359
iamjinpark <wldls7754@gmail.com>
2024-10-25 03:00:49 +0900

    fix : eslint 와 prettier 호환

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,36 @@
+import js from '@eslint/js';
 import globals from 'globals';
-import pluginJs from '@eslint/js';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
 import prettier from 'eslint-config-prettier';
 
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  prettier,
   {
+    ignores: ['dist'],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    extends: [
+      js.configs.recommended, // js 권장 설정 적용
+      ...tseslint.configs.recommended, // ts-eslint 권장 설정 적용
+      pluginReact.configs.flat.recommended, // react 권장 설정 적용
+      prettier, // prettier와 호환되도록 설정
+    ],
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
     rules: {
-      'prettier/prettier': 'error',
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'prettier/prettier': 'error', // Prettier 규칙 적용
     },
   },
 ];
